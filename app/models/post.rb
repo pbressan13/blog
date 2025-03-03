da70505edc5048023726f2892e73d583f720ff82
Pedro Bressan <pbressan13@gmail.com>
2025-03-03 12:06:48 -0300

    Adjusting other models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :ratings, dependent: :destroy
+
+  validates :title, :body, :ip, presence: true
+
+  def average_rating
+    ratings.average(:value).to_f.round(2)
+  end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,7 @@
 class Rating < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :value, presence: true, inclusion: { in: 1..5 }
+  validates :user_id, uniqueness: { scope: :post_id, message: "post only can be rated 1 time" }
 end
