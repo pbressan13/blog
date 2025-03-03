f98a53995664af89ec253606744f5575654aa4a3
Pedro Bressan <pbressan13@gmail.com>
2025-03-03 12:51:59 -0300

    Limiting routes, adding top route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :posts
+  resources :posts, only: [ :create ] do
+    collection do
+      get :top
+    end
+  end
   resources :ratings, only: [ :create ]
 end
