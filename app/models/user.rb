cef89a49b9255cef665e9aa8b68e8ff680aed45b
Pedro Bressan <pbressan13@gmail.com>
2025-03-03 12:02:40 -0300

    Adjusting rubocop and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :ratings, dependent: :destroy
+
+  validates :login, presence: true, uniqueness: true
 end
